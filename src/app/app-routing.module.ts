54709ac8b862b700686eb4334a94014f81940ba7
eduardo SANCHEZ <crowcl52@hotmail.com>
2019-08-26 21:23:00 -0500

    new update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OficinaComponent } from './oficina/oficina.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'oficina', component: OficinaComponent},
+  {path: 'proyectos', component: ProyectosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  { path: '**', redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OficinaComponent } from './oficina/oficina.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { HeaderComponent } from './header/header.component';
+import { ProyectoDetalleComponent } from './proyecto-detalle/proyecto-detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OficinaComponent,
+    ProyectosComponent,
+    ServiciosComponent,
+    HeaderComponent,
+    ProyectoDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
