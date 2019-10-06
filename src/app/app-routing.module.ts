b9921fc70ec27cb2599c3d4b5cd8d4d777ea865a
eduardo SANCHEZ <crowcl52@hotmail.com>
2019-10-05 20:50:28 -0500

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { OficinaComponent } from './oficina/oficina.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { ProyectoDetalleComponent } from './proyecto-detalle/proyecto-detalle.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'oficina', component: OficinaComponent},
   {path: 'proyectos', component: ProyectosComponent},
+  {path: 'proyectos/:id', component: ProyectoDetalleComponent},
   {path: 'servicios', component: ServiciosComponent},
   { path: '**', redirectTo: 'home' }
 
